e1c5fefaaa858df43b6b3c363239d21d92cfc350
Xinru Leng <xinruleng@gmail.com>
2024-04-03 19:28:53 -0700

    file name

--- copy-with-date-kotlin-native/src/nativeMain/kotlin/Main.kt
+++ copy-with-date-kotlin-native/src/nativeMain/kotlin/Main.kt
@@ -1,11 +1,23 @@
 import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
 fun main() {
-    println("Hello, Kotlin/Native!")
+    val name = "a.apk"
+    val newName = createNewName(name)
+    println("$name -> $newName")
+}
 
-    val currentMoment = Clock.System.now()
-    val dateTime = currentMoment.toLocalDateTime(TimeZone.currentSystemDefault())
-    println(dateTime.date)
+fun createNewName(name: String, date: LocalDate? = null): String {
+    val index = name.lastIndexOf(".")
+    val baseName = name.substring(0, index)
+    val dateStr = if (date == null) {
+        val currentMoment = Clock.System.now()
+        currentMoment.toLocalDateTime(TimeZone.currentSystemDefault()).date
+    } else {
+        date.toString()
+    }
+    val extension = name.substring(index)
+    return "$baseName-$dateStr$extension"
 }
