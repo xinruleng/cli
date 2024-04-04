f4b150da3c51f464dd5335d8e1448aedcf2745f3
Xinru Leng <xinruleng@gmail.com>
2024-04-03 19:34:31 -0700

    read command line arguments

--- copy-with-date-kotlin-native/src/nativeMain/kotlin/Main.kt
+++ copy-with-date-kotlin-native/src/nativeMain/kotlin/Main.kt
@@ -4,14 +4,25 @@ import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import okio.FileSystem
 import okio.Path.Companion.toPath
+import kotlin.system.exitProcess
 
-fun main() {
-    val name = "README.md"
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        println("Wrong command, usage: A app.apk")
+        exitProcess(1)
+    }
+    val name = args[0]
     val newName = createNewName(name)
-    println("$name -> $newName")
 
     val source = name.toPath()
     val target = newName.toPath()
+
+    val size = FileSystem.SYSTEM.metadata(source).size
+    if (size != null) {
+        println("$name -> $newName, size=$size")
+    } else {
+        println("$name -> $newName")
+    }
 
     FileSystem.SYSTEM.copy(source, target)
 }
