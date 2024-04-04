6dc674a21aebce40d3fa22abfa8ebfbdc20c62e8
Xinru Leng <xinruleng@gmail.com>
2024-04-03 19:26:32 -0700

    add date

--- copy-with-date-kotlin-native/src/nativeMain/kotlin/Main.kt
+++ copy-with-date-kotlin-native/src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,11 @@
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
 fun main() {
     println("Hello, Kotlin/Native!")
+
+    val currentMoment = Clock.System.now()
+    val dateTime = currentMoment.toLocalDateTime(TimeZone.currentSystemDefault())
+    println(dateTime.date)
 }
